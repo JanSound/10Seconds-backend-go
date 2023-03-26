e83f20191a18f0965fc2161037e1d260f872fc52
문상헌 <shmoon@toss.im>
2023-03-26 23:03:18 +0900

    feat: 배포 스크립트 추가

---
+++ scripts/rebuild_and_restart.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+IMAGE_NAME="tenseconds-go"
+CONTAINER_NAME="tenseconds-go"
+
+container_id=$(docker ps -aqf "name=${CONTAINER_NAME}")
+if [ ! -z "${container_id}" ]; then
+    docker stop ${CONTAINER_NAME}
+    docker rm "${container_id}"
+fi
+
+# 이미지 빌드
+docker build -t "${IMAGE_NAME}" .
+
+# 새 컨테이너 실행
+docker run -d --name "${CONTAINER_NAME}" -p 8001:8001 "${IMAGE_NAME}"
