6b47ac540f944bc67d375eb2b593a5e7495da3b3
문상헌 <shmoon@toss.im>
2023-04-29 20:39:50 +0900

    feat: presigned_url v2 로 가져오기

--- src/beat/presigned_url.go
+++ src/beat/presigned_url.go
@@ -1,11 +1,15 @@
 package beat
 
 import (
+	"context"
+	"log"
+
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/gin-gonic/gin"
+
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 
 	// swagger embed files
 	"fmt"
@@ -13,6 +17,40 @@ import (
 	"os"
 	"time"
 )
+
+type Presigner struct {
+	PresignClient *s3.PresignClient
+}
+
+func (presigner Presigner) GetObject(
+	bucketName string, objectKey string, lifetimeSecs int64) (*v4.PresignedHTTPRequest, error) {
+	request, err := presigner.PresignClient.PresignGetObject(context.TODO(), &s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(objectKey),
+	}, func(opts *s3.PresignOptions) {
+		opts.Expires = time.Duration(lifetimeSecs * int64(time.Second))
+	})
+	if err != nil {
+		log.Printf("Couldn't get a presigned request to get %v:%v. Here's why: %v\n",
+			bucketName, objectKey, err)
+	}
+	return request, err
+}
+
+func (presigner Presigner) PutObject(
+	bucketName string, objectKey string, lifetimeSecs int64) (*v4.PresignedHTTPRequest, error) {
+	request, err := presigner.PresignClient.PresignPutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(objectKey),
+	}, func(opts *s3.PresignOptions) {
+		opts.Expires = time.Duration(lifetimeSecs * int64(time.Second))
+	})
+	if err != nil {
+		log.Printf("Couldn't get a presigned request to put %v:%v. Here's why: %v\n",
+			bucketName, objectKey, err)
+	}
+	return request, err
+}
 
 func generateUniqueFilename() string {
 	now := time.Now()
@@ -22,52 +60,50 @@ func generateUniqueFilename() string {
 	return fmt.Sprintf("%s-%d", now.Format("20060102150405"), randomNumber)
 }
 
-func generatePutObjectPresignedURL(sess *session.Session, content_type string, objectKey string) (string, error) {
-	svc := s3.New(sess)
-	expiration := 180 * time.Minute // 서비스화 할 경우 조정할 예정
+// func generatePutObjectPresignedURL(sess *session.Session, content_type string, objectKey string) (string, error) {
+// 	svc := s3.New(sess)
+// 	expiration := 180 * time.Minute // 서비스화 할 경우 조정할 예정
 
-	bucket := os.Getenv("aws_s3_bucket")
+// 	bucket := os.Getenv("aws_s3_bucket")
 
-	req, _ := svc.PutObjectRequest(&s3.PutObjectInput{
-		Bucket:      aws.String(bucket),
-		Key:         aws.String(objectKey),
-		ContentType: aws.String(content_type),
-		// ContentType: aws.String("audio/x-m4a"), // or "audio/x-m4a"
-	})
-	presignedURL, err := req.Presign(expiration)
-	return presignedURL, err
-}
+// 	req, _ := svc.PutObjectRequest(&s3.PutObjectInput{
+// 		Bucket:      aws.String(bucket),
+// 		Key:         aws.String(objectKey),
+// 		ContentType: aws.String(content_type),
+// 		// ContentType: aws.String("audio/x-m4a"), // or "audio/x-m4a"
+// 	})
+// 	presignedURL, err := req.Presign(expiration)
+// 	return presignedURL, err
+// }
 
 // @Schemes
 // @Description create presigned url to upload beats (m4a audio file)
 // @Tags beats
 // @Router /beats/generate-presigned-url [post]
 func GeneratePutObjectPresignedURL(c *gin.Context) {
-	region := os.Getenv("aws_s3_region")
+
+	cfg, err := config.LoadDefaultConfig(
+		context.TODO(),
+		config.WithSharedConfigProfile("tenseconds"),
+		config.WithRegion("ap-northeast-2"),
+	)
+
+	if err != nil {
+		return
+	}
+	client := s3.NewFromConfig(cfg)
+	presignClient := s3.NewPresignClient(client)
+	presigner := Presigner{PresignClient: presignClient}
+	bucket := os.Getenv("aws_s3_bucket")
 	file_root := os.Getenv("aws_s3_file_root")
 	objectKey := file_root + generateUniqueFilename() + ".m4a"
-
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String(region),
-		Credentials: credentials.NewStaticCredentials(
-			os.Getenv("aws_access_key_id"), os.Getenv("aws_secret_access_key"), "",
-		),
-	})
+	presignedPutRequest, err := presigner.PutObject(bucket, objectKey, 60)
+	fmt.Printf(presignedPutRequest.URL)
 	if err != nil {
-		c.JSON(500, gin.H{
-			"error": fmt.Sprintf("Failed to create session: %v", err),
-		})
-		return
-	}
-	presignedURL, err := generatePutObjectPresignedURL(sess, "audio/x-m4a", objectKey)
-
-	if err != nil {
-		c.JSON(500, gin.H{
-			"error": fmt.Sprintf("Failed to generate presigned URL: %v", err),
-		})
-		return
+		panic(err)
 	}
 
+	presignedURL := presignedPutRequest.URL
 	c.JSON(200, gin.H{
 		"presigned_url": presignedURL,
 	})
