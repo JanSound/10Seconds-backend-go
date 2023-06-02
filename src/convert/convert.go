91667b2fc32e6014491a87aa8d5b5b1a89042c7d
문상헌 <shmoon@toss.im>
2023-06-03 05:18:09 +0900

    feat: add some stack stuff

--- src/convert/convert.go
+++ src/convert/convert.go
@@ -1,8 +1,11 @@
 package convert
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/JanSound/10Seconds-backend-go/beat"
@@ -37,18 +40,59 @@ func ConvertBeat(c *gin.Context) {
 func convertBeat(key string) []ConvertedBeatDTO {
 	// TODO(@shmoon): 음성 처리 서버로부터 3개의 응답 받아오기
 
-	tempPresignedUrl := getPresignedUrl(key)
+	convert(key)
+	presignedUrlList := getPresignedUrl(key)
 
 	beatList := []ConvertedBeatDTO{
-		{Key: key, BeatType: "base", PresignedUrl: tempPresignedUrl},
-		{Key: key, BeatType: "piano", PresignedUrl: tempPresignedUrl},
-		{Key: key, BeatType: "drum", PresignedUrl: tempPresignedUrl},
+		{Key: key, BeatType: "base", PresignedUrl: presignedUrlList[0]},
+		{Key: key, BeatType: "piano", PresignedUrl: presignedUrlList[1]},
+		{Key: key, BeatType: "drum", PresignedUrl: presignedUrlList[2]},
 	}
 
 	return beatList
 }
 
-func getPresignedUrl(key string) string {
+type ConvertDTO struct {
+	filename string
+}
+
+func convert(key string) {
+	url := os.Getenv("core_host") + "/beats/convert"
+	data := ConvertDTO{
+		filename: key,
+	}
+
+	payload, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return
+	}
+
+	// Create a request with the JSON payload
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	// Set the content type header
+	req.Header.Set("Content-Type", "application/json")
+
+	// Make the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Check the response status code
+	fmt.Println("Response status code:", resp.StatusCode)
+
+}
+
+func getPresignedUrl(key string) []string {
 	cfg, err := config.LoadDefaultConfig(
 		context.TODO(),
 		config.WithSharedConfigProfile("tenseconds"),
@@ -60,13 +104,19 @@ func getPresignedUrl(key string) string {
 	client := s3.NewFromConfig(cfg)
 	bucket := os.Getenv("aws_s3_bucket")
 
-	presignClient := s3.NewPresignClient(client)
-	presigner := beat.Presigner{PresignClient: presignClient}
+	ret := []string{}
+	myList := []string{"base", "piano", "drum"}
+	for _, value := range myList {
+		url := value + "/" + key
+		presignClient := s3.NewPresignClient(client)
+		presigner := beat.Presigner{PresignClient: presignClient}
 
-	presignedGetRequest, err := presigner.GetObject(bucket, key, 60)
-	if err != nil {
-		fmt.Println(err)
+		presignedGetRequest, err := presigner.GetObject(bucket, url, 60)
+		if err != nil {
+			fmt.Println(err)
+		}
+		ret = append(ret, presignedGetRequest.URL)
+
 	}
-	presignedURL := presignedGetRequest.URL
-	return presignedURL
+	return ret
 }
--- src/stack/stack.go
+++ src/stack/stack.go
@@ -3,5 +3,6 @@ package stack
 import "github.com/gin-gonic/gin"
 
 func StackBeat(c *gin.Context) {
+
 	c.JSON(200, gin.H{})
 }
